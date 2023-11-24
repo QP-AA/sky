ee0d699f58f98b5ba90df29191f247ab82c6bbdf
QP-AA <1697543774@qq.com>
2023-11-24 11:24:38 +0800

    用户下单

--- sky-server/src/main/java/com/sky/service/impl/OrderServiceImpl.java
+++ sky-server/src/main/java/com/sky/service/impl/OrderServiceImpl.java
@@ -4,11 +4,13 @@ import com.sky.constant.MessageConstant;
 import com.sky.context.BaseContext;
 import com.sky.dto.OrdersSubmitDTO;
 import com.sky.entity.AddressBook;
+import com.sky.entity.OrderDetail;
 import com.sky.entity.Orders;
 import com.sky.entity.ShoppingCart;
 import com.sky.exception.AddressBookBusinessException;
 import com.sky.exception.ShoppingCartBusinessException;
 import com.sky.mapper.AddressBookMapper;
+import com.sky.mapper.OrderDetailMapper;
 import com.sky.mapper.OrderMapper;
 import com.sky.mapper.ShoppingCartMapper;
 import com.sky.service.OrderService;
@@ -16,8 +18,10 @@ import com.sky.vo.OrderSubmitVO;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -31,7 +35,11 @@ public class OrderServiceImpl implements OrderService {
 
     @Autowired
     private ShoppingCartMapper shoppingCartMapper;
+
+    @Autowired
+    private OrderDetailMapper orderDetailMapper;
     @Override
+    @Transactional
     public OrderSubmitVO submit(OrdersSubmitDTO ordersSubmitDTO) {
 
         // 判断地址和购物车是否为空
@@ -57,5 +65,23 @@ public class OrderServiceImpl implements OrderService {
 
         orderMapper.insert(orders); // 向订单中插入数据
 
+        List<OrderDetail> orderDetails = new ArrayList<>();
+        for (ShoppingCart shoppingCart : shoppingCarts) {
+            OrderDetail detail = new OrderDetail();
+            BeanUtils.copyProperties(shoppingCart, detail);
+            detail.setOrderId(orders.getId());
+            orderDetails.add(detail);
+        }
+        orderDetailMapper.insertBatch(orderDetails); // 订单明细中批量插入数据
+
+        shoppingCartMapper.clean(BaseContext.getCurrentId()); // 删除购物车中数据
+
+        OrderSubmitVO submitVO = OrderSubmitVO.builder()
+                .id(orders.getId())
+                .orderTime(orders.getOrderTime())
+                .orderNumber(orders.getNumber())
+                .orderAmount(orders.getAmount())
+                .build();
+        return submitVO;
     }
 }
