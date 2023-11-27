4362a9fcc4d8fa551936c9570afa12b74ea52bc0
QP-AA <1697543774@qq.com>
2023-11-27 10:28:49 +0800

    跳过微信支付v2

--- sky-server/src/main/java/com/sky/service/impl/OrderServiceImpl.java
+++ sky-server/src/main/java/com/sky/service/impl/OrderServiceImpl.java
@@ -121,15 +121,16 @@ public class OrderServiceImpl implements OrderService {
         OrderPaymentVO vo = jsonObject.toJavaObject(OrderPaymentVO.class);
 
         vo.setPackageStr(jsonObject.getString("package"));
-        //为替代微信支付成功后的数据库订单状态更新，多定义一个方法进行修改
-
-        Integer OrderPaidStatus = Orders.PAID; //支付状态，已支付
-
-        Integer OrderStatus = Orders.TO_BE_CONFIRMED;  //订单状态，待接单
-        //发现没有将支付时间 check_out属性赋值，所以在这里更新
-        LocalDateTime check_out_time = LocalDateTime.now();
-        Orders order = orderMapper.getByNumber(ordersPaymentDTO.getOrderNumber());
-        orderMapper.updateStatus(OrderStatus, OrderPaidStatus, check_out_time, order.getId());
+        paySuccess(ordersPaymentDTO.getOrderNumber());
+//        //为替代微信支付成功后的数据库订单状态更新，多定义一个方法进行修改
+//
+//        Integer OrderPaidStatus = Orders.PAID; //支付状态，已支付
+//
+//        Integer OrderStatus = Orders.TO_BE_CONFIRMED;  //订单状态，待接单
+//        //发现没有将支付时间 check_out属性赋值，所以在这里更新
+//        LocalDateTime check_out_time = LocalDateTime.now();
+//        Orders order = orderMapper.getByNumber(ordersPaymentDTO.getOrderNumber());
+//        orderMapper.updateStatus(OrderStatus, OrderPaidStatus, check_out_time, order.getId());
 
         return vo;
     }
