dc03ae612000fd75da1c74072186e2f0f76cad00
QP-AA <1697543774@qq.com>
2023-11-29 21:07:07 +0800

    工作台

--- sky-server/src/main/java/com/sky/mapper/DishMapper.java
+++ sky-server/src/main/java/com/sky/mapper/DishMapper.java
@@ -12,6 +12,7 @@ import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
 
 import java.util.List;
+import java.util.Map;
 
 @Mapper
 public interface DishMapper {
@@ -45,4 +46,11 @@ public interface DishMapper {
     public List<Dish> getByCategoryId(Long categoryId);
 
     List<Dish> list(Dish dish);
+
+    /**
+     * 根据条件统计菜品数量
+     * @param map
+     * @return
+     */
+    Integer countByMap(Map map);
 }
